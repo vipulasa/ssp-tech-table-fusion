af5c76d46f8ee583f7fb466788db398b9d238526
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2024-05-09 14:14:01 +0530

    Add /hello, /test-error, and /payload routes
    This commit adds three new routes to the application: /hello, /test-error and /payload routes. The /hello route returns a simple greeting message in json format. The /test-error route captures errors and returns them in json format. The /payload route returns status, greetings, and an array in json format.

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,61 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})
+    ->middleware('auth:sanctum');
+
+
+Route::get('/hello', function (){
+    return response()->json([
+        'name' => 'Laravel',
+        'greeting' => 'Hello, Laravel!',
+        'array' => [1, 2, 3, 4, 5, 6]
+    ]);
+});
+
+
+Route::get('/test-error', function () {
+
+    return rescue(function(){
+        // this is where you write the code !!
+
+        throw new \Exception('This is an exception');
+
+    }, function(\Exception $exception){
+        // this is where you capture the errors !!!
+
+        return response()->json([
+            'status' => false,
+            'payload' => [
+                'message' => $exception->getMessage()
+            ],
+            '_time' => time()
+        ], 500);
+    });
+
+    try {
+
+        throw new \Exception('This is an exception');
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => $e->getMessage()
+        ]);
+    }
+});
+
+
+Route::get('/payload', function () {
+
+    return response()->json([
+        'status' => true,
+        'payload' => [
+            'name' => 'Laravel',
+            'greeting' => 'Hello, Laravel!',
+            'array' => [1, 2, 3, 4, 5, 6]
+        ],
+        '_time' => time()
+    ]);
+
+
+});
